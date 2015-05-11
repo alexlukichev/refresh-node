4cff07690b0d1548cc12a20997be12d237abec7a
Alexander Lukichev <alexander.s.lukichev@gmail.com>
2015-05-11 17:23:22 -0500

    initial commit

---
+++ runnode.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function start_node() {
+  if [ -f .lock ]; then
+    return
+  fi
+  touch .lock
+  echo "(Re)starting node..."
+  npm install && npm prune
+#  while netstat -ln | grep :5000; do 
+#    echo "waiting for port 5000..."
+#    sleep 0.5; 
+#  done
+  node server.js & echo $! > .pid
+  echo "node started, pid=`cat .pid`"
+  rm -f .lock
+}
+
+rm -f .lock
+start_node
+sleep 1
+while true; do
+  if [ -f .restart ]; then
+    if [ -f .pid ]; then
+      kill -2 `cat .pid` && rm -f .pid .restart
+    fi
+  fi
+  if [ ! -f .pid ] || [ ! ps -p `cat .pid` >/dev/null 2>&1 ]; then
+    rm -f .pid
+    start_node
+  fi
+  sleep 1
+done
